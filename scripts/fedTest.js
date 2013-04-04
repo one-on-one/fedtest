5340175768a307db31743cd2f6cc32ab83fc5734
Tyler Ipson <tyler@implemint.com>
2013-04-04 01:43:21 -0600

    Adding in javascript form validation. I have disabled the default form validation via novalidate for testing purposes, but it would ideally be left in place and use this js as a fallback for dated browsers.

--- scripts/fedTest.js
+++ scripts/fedTest.js
@@ -1,10 +1,23 @@
-var edu = document.getElementById('edu'),
+function educationFields() {
+
+	var edu = document.getElementById('edu'),
 	edufields = document.getElementById('edufields');
 
-// Hide the field container so we can toggle it later
-edufields.style.display = 'none';
+	// Hide the field container so we can toggle it later
+	edufields.style.display = 'none';
 
-function getEduLevels(data) {
+	edu.onclick = function() {
+		// Toggle the field container
+		edufields.style.display = (edufields.style.display != 'none' ? 'none' : '' );
+	}
+
+	// Education levels can change, let's pull them in with the API
+	var script = document.createElement('script');
+	script.src = 'http://fedtest.aws.af.cm/?callback=getEduJSON';
+	document.body.appendChild(script);
+}
+
+function getEduJSON(data) {
 
 	// Return array of JSONP call
 	var arr = [data];
@@ -35,12 +48,63 @@ function getEduLevels(data) {
 	}
 }
 
-edu.onclick = function() {
-	// Toggle the field container
-	edufields.style.display = (edufields.style.display != 'none' ? 'none' : '' );
-}
+// Call educationFields when loaded
+educationFields();
 
-// Education levels can change, let's pull them in with the API
-var script = document.createElement('script');
-script.src = 'http://fedtest.aws.af.cm/?callback=getEduLevels';
-document.body.appendChild(script);
+// Form validation
+function validate(frm){
+	var validForm = true,
+		errMsg = '';
+	
+	// For all fields with required attribute
+	for(i=0; i<frm.elements.length; i++){
+		var elem = frm.elements[i];
+		if(elem.getAttribute('required') && elem.value == '') {
+			errMsg += '<li>' + elem.getAttribute('data-required-message') + '</li>';
+			validForm = false;
+		}
+	}
+
+	// 1. Get phone value and strip it so there's just numbers
+	// 2. Get email value and set regex filter to test against
+	// 3. Get the value of education level fields
+	var phone = frm.elements['phone'].value.replace(/[^\d]/g,''),
+		email = frm.elements['email'].value,
+		filter = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+		education = frm.elements['education-level'].value;
+
+	// Throw error if phone strips to anything other than 10 chars
+	if(phone.length != 10) {
+		errMsg += '<li>Please enter a valid phone number, eg: (555) 555-5555</li>';
+		validForm = false;
+	} else {
+		// Otherwise we're good, now format the field the way we need it
+		frm.elements['phone'].value = '(' + phone.substring(0,3) + ") " + phone.substring(3, 6) + "-" + phone.substring(6);
+	}
+	
+	// Test the email against the regex
+	if(!filter.test(email)) {
+		errMsg += '<li>Please enter a valid email address, eg: yourname@domain.com</li>';
+		validForm = false;
+	}
+
+	// Not sure why these weren't included with the global check
+	if(education == '') {
+		errMsg += '<li>Please select your current education level.</li>';
+		validForm = false;
+	}
+
+	// If there's an error of any kind, let the user know
+	if(!validForm){
+		//alert(errMsg);
+		var messages = document.getElementById('messages'),
+			errElem = document.createElement('ol');
+
+		errElem.innerHTML = errMsg;
+		messages.appendChild(errElem);
+		//document.getElementById('messages').appendChild('<p>'+errMsg+'</p>');
+		return false;
+	}
+	
+	return true;
+}
