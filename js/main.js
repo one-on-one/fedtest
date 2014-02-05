a97917cc382ea8d357a117a9014d7d9228d922b2
Michael Richins <mr.grigri@gmail.com>
2014-02-05 13:53:49 -0700

    Added last name validation
    I added the last name validation

--- js/main.js
+++ js/main.js
@@ -27,10 +27,15 @@ function calcFields() {
 	var bachelor_value    = bachelor.value;
 	var master_value      = master.value;
 	var doctoral_value    = doctoral.value;
-	
+
 	/* Validation statements for names, phone numbers and email */
 	if ( first_name_value == null || first_name_value == "" || !(name_cr.test(first_name_value)) ) {
 		alert("Please enter a valid first name.");
+		return false;
+	}
+
+	if ( last_name_value == null || last_name_value == "" || !(name_cr.test(last_name_value)) ) {
+		alert("Please enter a valid last name.");
 		return false;
 	}
 }
