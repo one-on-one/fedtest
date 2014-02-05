e38affe69f5684680bd85177aa15ceb1111005ed
Michael Richins <mr.grigri@gmail.com>
2014-02-05 13:45:30 -0700

    Added variables
    I added the variables to identify each element by their id

--- js/main.js
+++ js/main.js
@@ -3,4 +3,16 @@ function calcFields() {
 	var name_cr  = /^[A-Za-z]{1,}$/;
 	var phone_cr = /^\([0-9]{3}\)\ [0-9]{3}\-[0-9]{4}$/;
 	var email_cr = /^[A-Za-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/;
+
+	/* Elements by ID */
+	var first_name = document.getElementById("first_name");
+	var last_name  = document.getElementById("last_name");
+	var phone      = document.getElementById("phone");
+	var email      = document.getElementById("email");
+
+	var high_school = document.getElementById("high_school");
+	var associate   = document.getElementById("associate");
+	var bachelor    = document.getElementById("bachelor");
+	var master      = document.getElementById("master");
+	var doctoral    = document.getElementById("doctoral");
 }
