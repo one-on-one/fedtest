b7da412351206d4f492682e3114d67e4c9a74ba0
Michael Richins <mr.grigri@gmail.com>
2014-02-05 13:32:42 -0700

    Added criteria vaiables
    I added the variables that will hold the different criteria for the
    names, phone number, and email.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
 function calcFields() {
-	
+	/* Criteria for names, phone number, and email */
+	var name_cr  = /^[A-Za-z]{1,}$/;
+	var phone_cr = /^\([0-9]{3}\)\ [0-9]{3}\-[0-9]{4}$/;
+	var email_cr = /^[A-Za-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/;
 }
