51397daa994ce03223b9c59be182fbba2e7cec2d
unknown <nndustriemedia@gmail.com>
2014-02-10 01:13:41 -0700

    removed an unnecessary div element from the education inputs
    added an id to each label for use in validation.
    
    added a call to the function validate for each label's onblur event, except for the education radio buttons, radio buttons are checked only when an atempt is made to submit the form.
    
    added a call to the the function beforeSubmit.
    
    when validation fails, the class "error" is added to the label, causing the css to display the title as a message to the user.
    
    before submitting the the form, we run through the form one last time to verify each input is filed out and that no label has the class "error".

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// jsonp call 
 function update_form (data) {
 var edu = document.getElementById("education");
   for(var key in data){
@@ -21,3 +22,70 @@ window.onload=function(){
 	script_element.src = 'http://fedtest.aws.af.cm/?callback=update_form';
 	document.getElementsByTagName('head')[0].appendChild(script_element);
 };
+
+// End jsonp call 
+
+
+// form validation
+var err = 0;
+function validate(e){
+	var val = document.getElementById(e).value;
+	switch (e)
+	{
+	case "fname":
+	  if(!val){err = 0; }else{ err = 1; }
+	  checkError(e);
+	  break;
+	case "lname":
+	  if(!val){err = 0; }else{ err = 1; }
+	  checkError(e);
+	  break;
+	case "phone":
+	  var phoneno = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;  
+	  if(!val.match(phoneno)){err = 0; }else{ err = 1; }
+	  checkError(e);
+	  break;
+	case "email":
+	  var email = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+	  if(!val.match(email)){err = 0; }else{ err = 1; }
+	  checkError(e);
+	  break;
+	}		
+};
+
+function checkError(e){
+	  var error = document.getElementById(e+'Error');
+  	  var errorClass = error.className;
+	 if(errorClass == "validate" && err == 0){
+	    	error.className = errorClass + " error";
+	    }
+	else if(errorClass == "validate error" && err >= 1){
+	    	error.className = errorClass = "";
+	    	error.className = errorClass + "validate";
+	    }
+}
+
+function lastCheck(){   
+   var radio = document.getElementsByName('edu');
+   var isradio;
+   for (var i=0; i<radio.length; i++) {
+		if (radio[i].checked) {
+			isradio = "checked";
+	    }
+	}
+	
+	if(isradio !== "checked"){err = 0; }else{ err = 1; }
+	  checkError('radio');
+		
+	var valarr = new Array("fname","lname","phone", "email");
+	for (i=0; i < valarr.length; i++) {
+		validate(valarr[i]);
+  	}  	  	
+}
+
+function beforeSubmit(){
+	lastCheck();
+	err = document.querySelectorAll('.error').length;
+	if(err == 0 ){ return true; }else{ return false; }
+}
+// end form validation
