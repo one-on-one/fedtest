b6ccbe9750b6db15f0b1d7bce86b349987e2c08d
Keith Smith <ksmith.ut@gmail.com>
2014-02-20 12:46:49 -0700

    initial angular app setup for form

---
+++ scripts/main.js
@@ -0,0 +1,33 @@
+// Set up the base App
+angular.module('FedTestApp', [
+  'FedTestApp.Controllers',
+  'FedTestApp.Services'
+]);
+angular.module('FedTestApp.Controllers', []);
+angular.module('FedTestApp.Services', []);
+
+// Controllers
+// ===========
+
+// MainFormController
+// ------------------
+angular.module('FedTestApp.Controllers')
+.controller('MainFormController', [
+  '$scope',
+  function ($scope) {
+
+  }
+]);
+
+// Services
+// ========
+
+//
+// ---
+angular.module('FedTestApp.Services')
+.service('', [
+  '$http',
+  function ($http) {
+
+  }
+]);
